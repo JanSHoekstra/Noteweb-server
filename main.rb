abc3c6f8a6ee0a4f4b7ec1d195559a6f9a99ae4f
Thimo <tsdezwart@protonmail.com>
2020-11-23 09:46:26 +0100

    Update syntax for Ruby 2.9

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'sinatra/base'
 require 'sinatra/json'
@@ -7,34 +8,36 @@ require 'webrick/https'
 require 'openssl'
 
 webrick_options = {
-    :Port               => 443,
-    :Logger             => WEBrick::Log::new($stderr, WEBrick::Log::DEBUG),
-    :DocumentRoot       => "/ruby/htdocs",
-    :SSLEnable          => true,
-    :SSLVerifyClient    => OpenSSL::SSL::VERIFY_NONE,
-    :SSLCertificate     => OpenSSL::X509::Certificate.new(File.open("alt.cert").read),
-    :SSLPrivateKey      => OpenSSL::PKey::RSA.new(        File.open("alt.key").read),
-    :SSLCertName        => [ [ "CN",WEBrick::Utils::getservername ] ]
+  Port: 2048,
+  Logger: WEBrick::Log.new($stderr, WEBrick::Log::DEBUG),
+  DocumentRoot: '/ruby/htdocs',
+  SSLEnable: true,
+  SSLVerifyClient: OpenSSL::SSL::VERIFY_NONE,
+  SSLCertificate: OpenSSL::X509::Certificate.new(File.open('alt.cert').read),
+  SSLPrivateKey: OpenSSL::PKey::RSA.new(File.open('alt.key').read),
+  SSLCertName: [['CN', WEBrick::Utils.getservername]]
 }
 
+# The server
 class MyServer < Sinatra::Base
-    #set :environment, :production
+  # set :environment, :production
 
-    get '/' do
-        json text: "Hellow, world!"
-    end
+  get '/' do
+    json text: 'Hellow, world!'
+  end
 
-    get '/hello' do
-        json lol: "bye"
-    end
+  get '/hello' do
+    json lol: 'bye'
+  end
 
-    post '/login' do
-        if params.has_key(:name) && params.has_key?(:pass) then
-            json text: "Success!"
-        else
-            json text: "Bad request!"
-        end
+  post '/login' do
+    if params.key?(:name) && params.key?(:pass)
+      json text: 'Success!'
+    else
+      json text: 'Bad request!'
     end
+  end
 end
 
 Rack::Handler::WEBrick.run MyServer, webrick_options
+# vim: tabstop=2 shiftwidth=2 expandtab
