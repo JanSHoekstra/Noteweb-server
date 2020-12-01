17272bc9f2996ed9ef6c46689b037db962ff1e43
Jan Hoekstra <altlockg@gmail.com>
2020-12-01 11:23:46 +0100

    Jan Itor has come to the rescue.

--- classes/book.rb
+++ classes/book.rb
@@ -1,41 +1,48 @@
 # frozen_string_literal: true
 
 require 'httparty'
-
 load 'classes/helper.rb'
 
 # Book object containing information about books
 class Book
-
   def set_goodreads_key
     goodreads_path = 'db/goodreads.json'
+
     if File.exist?(goodreads_path)
-      goodreads_key = (JSON.parse(File.open(goodreads_path).read))['apikey']
-    else
-      puts 'Goodreads key not found! Please enter it below:'
-      goodreads_key = gets.chomp
-      Dir.mkdir('db') unless Dir.exist?('db')
-      File.open(goodreads_path, 'w') { |f| f.write("{\"apikey\": \"#{goodreads_key}\"}") }
+      return (JSON.parse(File.open(goodreads_path).read))['apikey']
     end
-    return goodreads_key
+
+    puts 'Goodreads key not found! Please enter it below:'
+    goodreads_key = gets.chomp
+    Dir.mkdir('db') unless Dir.exist?('db')
+    File.open(goodreads_path, 'w') { |f| f.write("{\"apikey\": \"#{goodreads_key}\"}") }
+    goodreads_key
   end
 
   def get_rating(goodreads_key)
     # 'curl -X GET -F 'key=<key>' -F 'isbns=0824985990' -F 'format=json' https://www.goodreads.com/book/review_counts.json'
-    unless @isbn == ''
-      query = {
-        'key': goodreads_key,
-        'isbns': @isbn,
-        'format': 'json'
-      }
-      goodreads_data = JSON.parse(HTTParty.get('https://www.goodreads.com/book/review_counts.json', :query => query).to_s)
-      @rating = value_of(goodreads_data['books'][0], 'average_rating').to_f
-    end
+    return '' if @isbn == '' || @isbn.nil?
+
+    query = {
+      'key': goodreads_key,
+      'isbns': @isbn,
+      'format': 'json'
+    }
+    # 'No ISBNs specified.'
+    reviews = HTTParty.get('https://www.goodreads.com/book/review_counts.json', query: query)
+    goodreads_data = JSON.parse(reviews.to_s)
+    value_of(goodreads_data['books'][0], 'average_rating').to_f
   end
 
   def get_wiki(search)
-    result = uri_to_json('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + search)
+    result = uri_to_json("https://en.wikipedia.org/w/api.php?action=opensearch&search=#{search}")
     value_of(value_of(result, 3), 0)
+  end
+
+  def author_data(work_data)
+    authors = value_of(work_data, 'authors')
+    author_data = uri_to_json("https://openlibrary.org#{authors[0]['key']}.json")
+    author_data || uri_to_json("https://openlibrary.org#{authors[0]['author']['key']}.json")
   end
 
   def initialize(openlibrary_id)
@@ -49,28 +56,21 @@ class Book
     @publish_date = value_of(work_data, 'publish_date')
     @amazon_id = value_of(value_of(work_data, 'identifiers'), 'amazon').to_s.delete '["]'
     @amazon_link = @amazon_id == '' ? '' : "https://www.amazon.com/dp/#{amazon_id}"
-
-    # Getting author depends on the way it's stored in openlibrary
-    if work_data['authors']
-      begin
-        author_data = uri_to_json("https://openlibrary.org#{work_data['authors'][0]['key']}.json")
-      rescue
-        author_data = uri_to_json("https://openlibrary.org#{work_data['authors'][0]['author']['key']}.json")
-      end
-      @author = value_of(author_data, 'name')
-      # remove /authors/ from /authors/<author_id>
-      @author_id = author_data['key'].delete('/authors/')
-    else
-      @author = value_of(work_data, 'by_statement')
-    end
-
     @author_wiki = get_wiki(@author)
     @book_wiki = get_wiki(@title)
-    @isbn = (value_of(book_data, 'isbn_10'))[0]
-    get_rating(set_goodreads_key)
+    @isbn = value_of(value_of(book_data, 'isbn_10'), 0)
+    @rating = get_rating(set_goodreads_key)
+
+    # Getting author depends on the way it's stored in openlibrary
+    author_data_var = author_data(work_data)
+    @author = value_of(author_data_var, 'name')
+    @author = value_of(work_data, 'by_statement') if @author == ''
+    # remove /authors/ from /authors/<author_id>
+    @author_id = value_of(author_data_var, 'key').delete('/authors/')
   end
 
-  attr_reader :id, :title, :author_id, :author, :description, :subjects, :publish_date, :amazon_id, :amazon_link, :author_wiki, :book_wiki, :rating
+  attr_reader :id, :title, :author_id, :author, :description, :subjects,
+    :publish_date, :amazon_id, :amazon_link, :author_wiki, :book_wiki, :rating
 
   def print_details
     puts "Id: #{@id}"
@@ -87,5 +87,4 @@ class Book
   def to_hash
     Hash[instance_variables.map { |var| [var.to_s[1..-1], instance_variable_get(var)] }]
   end
-
 end
--- classes/helper.rb
+++ classes/helper.rb
@@ -1,23 +1,26 @@
 # frozen_string_literal: true
 
 require 'httparty'
+require 'json'
 
 def value_of(array, value)
-  array.nil? || array[value].nil? ? '' : array[value]
+  !array || array[value].nil? ? '' : array[value]
 end
 
 def uri_to_json(uri)
-  JSON.parse(HTTParty.get(URI(uri)).to_s) ? JSON.parse(HTTParty.get(URI(uri)).to_s) : ''
+  JSON.parse(HTTParty.get(URI(uri)).to_s)
+rescue HTTParty::ResponseError => e
+  puts e
+  false
 rescue JSON::ParserError => e
   puts e
-  ''
+  false
 end
 
 def recommend(author = '', subject = '')
-  search_template = 'https://openlibrary.org/search.json'
   query = {
     'author': author,
     'subject': subject
   }
-  search_data = JSON.parse(HTTParty.get('https://openlibrary.org/search.json', :query => query).to_s)
+  JSON.parse(HTTParty.get(site: 'https://openlibrary.org/search.json', query: query).to_s)
 end
