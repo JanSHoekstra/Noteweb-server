9966ec7a14bf3e77b84c5d5c386f15ad9b835d4c
Thimo <tsdezwart@protonmail.com>
2021-01-13 13:41:47 +0100

    Restart server into production mode

--- scripts/restartServer.sh
+++ scripts/restartServer.sh
@@ -6,11 +6,11 @@ then
         echo "Server has been restarted."
         pid=$(ps -e | grep -i ruby | { read a _; echo "$a"; })
         kill $pid
-        ruby /home/henkdevries/socialreadingappserver/main.rb &
+        ruby /home/henkdevries/socialreadingappserver/main.rb -p &
         disown -a
 else
         echo "Server was not running, has been started now."
-        ruby /home/henkdevries/socialreadingappserver/main.rb &
+        ruby /home/henkdevries/socialreadingappserver/main.rb -p &
         disown -a
 fi
 
