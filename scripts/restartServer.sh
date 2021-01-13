c540b40094f5a81050955d872d908e0d862ce2b6
Thimo <tsdezwart@protonmail.com>
2021-01-13 11:40:18 +0100

    Print more details in scripts

--- scripts/restartServer.sh
+++ scripts/restartServer.sh
@@ -3,11 +3,13 @@
 rubycheck=$(ps -e | grep -i ruby | wc -l)
 if [ $rubycheck -eq 1 ]
 then
+        echo "Server has been restarted."
         pid=$(ps -e | grep -i ruby | { read a _; echo "$a"; })
         kill $pid
         ruby /home/henkdevries/socialreadingappserver/main.rb &
         disown -a
 else
+        echo "Server was not running, has been started now."
         ruby /home/henkdevries/socialreadingappserver/main.rb &
         disown -a
 fi
--- scripts/stopServer.sh
+++ scripts/stopServer.sh
@@ -3,7 +3,10 @@
 rubycheck=$(ps -e | grep -i ruby | wc -l)
 if [ $rubycheck -eq 1 ]
 then
+        echo "Server has been stopped."
         pid=$(ps -e | grep -i ruby | { read a _; echo "$a"; })
         kill $pid
+else
+        echo "Server was not running."
 fi
 
